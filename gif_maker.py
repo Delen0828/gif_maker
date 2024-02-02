c7c23858e7be23fb11454ed926d9d71ee23d49e3
Delen0828 <hsw828@sjtu.edu.cn>
2024-02-02 02:02:01 -0500

    update

--- gif_maker.py
+++ gif_maker.py
@@ -4,6 +4,9 @@ import numpy as np
 # import pandas as pd
 import random
 import pickle
+SAMPLE_TIME=1
+DURATION=100
+SPIKE_H=10
 
 with open('mean.pkl','rb') as f:
 	mean_list=pickle.load(f)
@@ -14,52 +17,192 @@ def xList_create(duration):
 def yList_create(index,avg,var,spike,spike_h,duration):
 	baseline=np.ones(duration)*avg
 	variation=np.random.random(duration)*var
-	if (spike[0] > 1):
+	if spike is True:
 		variation[np.random.randint(duration)]+=spike_h*var
 	return baseline+variation
 
-DURATION=100
+
+###############
+### 2 Lines ###
+###############
+
+def sampler_2line(cfg,high_data,low_data):
+	if cfg == 'hh':
+		return random.sample(high_data,1)[0],random.sample(high_data,1)[0]
+	elif cfg == 'hl':
+		return random.sample(high_data,1)[0],random.sample(low_data,1)[0]
+	elif cfg == 'lh':
+		return random.sample(low_data,1)[0],random.sample(high_data,1)[0]
+	elif cfg == 'll':
+		return random.sample(low_data,1)[0],random.sample(low_data,1)[0]
+	else:
+		return None
+	
 LINE_NUM=2
-#TODO: Devide AVG sample in to low medium high based on line 2,3,4
-#TODO: Same for VAR
-#TODO Based on low/high 
-# AVG_LIST=[[-5,5],[5,-5],[-5,-5],[5,5]]
-# VAR_LIST=[[6,3],[3,6],[3,3],[6,6]]
-# SPIKE_LIST=[[0,1],[1,0],[0,0],[1,1]]
-AVG_HIGH2=[i for i in mean_list if i > np.median(mean_list)]
-AVG_LOW2=[i for i in mean_list if i < np.median(mean_list)]
-# print(AVG_LOW)
-SPIKE_H=10
-CBF_PALLETE=['#648FFF','#DC267F','#FE6100','#FFB000'] 
-CBF_NAME=['b','r','o','y']
-# for AVG in AVG_LIST:
-# 	for VAR in VAR_LIST:
-# 		for SPIKE in SPIKE_LIST:
-fig = plt.figure()
-xList=[] #Length of the line
-yList=[] #function that INPUT: AVG,VAR,SPIKE,OUTPUT: A series of num
-lineList=[]
-pallete=random.sample(CBF_PALLETE,k=LINE_NUM)
-colorName=''
-for color in pallete:
-	colorName+=(CBF_NAME[CBF_PALLETE.index(color)])
-for i in range(LINE_NUM):
-	l, = plt.plot([], [], color=pallete[i])
-	AVG=int(random.sample(mean_list,1)[0])
-	VAR=int(random.sample(var_list,1)[0])
-	SPIKE=random.sample([1,5],1)
-	xList.append(xList_create(DURATION))
-	yList.append(yList_create(i,AVG,VAR,SPIKE,SPIKE_H,DURATION))
-	plt.xlim(0,DURATION) #depend on Duration
-	plt.ylim(0, 500)
-	lineList.append(l)
-plt.close()
+AVG_HIGH=[i for i in mean_list if i > np.median(mean_list)]
+AVG_LOW=[i for i in mean_list if i < np.median(mean_list)]
+AVG_CFG2=['hh','hl','lh','ll']
+VAR_HIGH=[i for i in var_list if i > np.median(var_list)]
+VAR_LOW=[i for i in var_list if i < np.median(var_list)]
+VAR_CFG2=['hh','hl','lh','ll']
+SPIKE_CFG2=['hh','hl','lh','ll']
+CFG_LIST=[]
 
-writer = anime.PillowWriter(fps=50)
-
-with writer.saving(fig, f"gif/trace_AVG{AVG}_VAR{VAR}_SPIKE{SPIKE}_{colorName}.gif", 100):
+IBM_PALLETE=['#648FFF','#DC267F','#FE6100','#FFB000'] 
+COLOR_NAME=['b','r','o','y']
+for acfg in AVG_CFG2:
+	for vcfg in VAR_CFG2:
+		for scfg in SPIKE_CFG2:
+			CFG_LIST.append((acfg,vcfg,scfg))
+			
+def draw(cfg):
+	acfg=cfg[0]
+	vcfg=cfg[1]
+	scfg=cfg[2]		
+	fig = plt.figure()
+	xList=[] #Length of the line
+	yList=[] #function that INPUT: AVG,VAR,SPIKE,OUTPUT: A series of num
+	lineList=[]
+	pallete=IBM_PALLETE[:LINE_NUM]
+	colorName=''
+	for color in pallete:
+		colorName+=(COLOR_NAME[IBM_PALLETE.index(color)])
+	AVG=sampler_2line(acfg,AVG_HIGH,AVG_LOW)
+	VAR=sampler_2line(vcfg,VAR_HIGH,VAR_LOW)
+	SPIKE=sampler_2line(scfg,[True],[False])
 	for i in range(LINE_NUM):
-		for j in range(DURATION):
-			lineList[i].set_data(xList[i][:j], yList[i][:j])
-			writer.grab_frame()
-# print(DURATION*LINE_NUM/50)
+		l, = plt.plot([], [], color=pallete[i])
+		xList.append(xList_create(DURATION))
+		yList.append(yList_create(i,AVG[i],VAR[i],SPIKE[i],SPIKE_H,DURATION))
+		plt.xlim(0,DURATION) #depend on Duration
+		plt.ylim(0,500) #TODO: How to determine the upper bound?
+		lineList.append(l)
+	plt.close()
+
+	writer = anime.PillowWriter(fps=50)
+
+	with writer.saving(fig, f"gif/trace/line2/trace_AVG{acfg}_VAR{vcfg}_SPIKE{scfg}_{colorName}.gif", 100):
+		for i in range(LINE_NUM):
+			for j in range(DURATION):
+				lineList[i].set_data(xList[i][:j], yList[i][:j])
+				writer.grab_frame()
+
+###############
+### 3 Lines ###
+###############
+
+def sampler_3line(cfg,high_data,low_data):
+	if cfg == 'hhh':
+		return random.sample(high_data,1)[0],random.sample(high_data,1)[0],random.sample(high_data,1)[0]
+	elif cfg == 'hll':
+		return random.sample(high_data,1)[0],random.sample(low_data,1)[0],random.sample(low_data,1)[0]
+	elif cfg == 'lhl':
+		return random.sample(low_data,1)[0],random.sample(high_data,1)[0],random.sample(low_data,1)[0]
+	elif cfg == 'llh':
+		return random.sample(low_data,1)[0],random.sample(low_data,1)[0],random.sample(high_data,1)[0]
+	elif cfg == 'lll':
+		return random.sample(low_data,1)[0],random.sample(low_data,1)[0],random.sample(low_data,1)[0]
+	else:
+		return None
+	
+LINE_NUM=3
+AVG_HIGH=[i for i in mean_list if i > np.median(mean_list)]
+AVG_LOW=[i for i in mean_list if i < np.median(mean_list)]
+AVG_CFG3=['hhh','hll','lhl','llh','lll']
+VAR_HIGH=[i for i in var_list if i > np.median(var_list)]
+VAR_LOW=[i for i in var_list if i < np.median(var_list)]
+VAR_CFG3=['hhh','hll','lhl','llh','lll']
+SPIKE_CFG3=['hhh','hll','lhl','llh','lll']
+
+
+IBM_PALLETE=['#648FFF','#DC267F','#FE6100','#FFB000'] 
+COLOR_NAME=['b','r','o','y']
+for acfg in AVG_CFG3:
+	for vcfg in VAR_CFG3:
+		for scfg in SPIKE_CFG3:
+			fig = plt.figure()
+			xList=[] #Length of the line
+			yList=[] #function that INPUT: AVG,VAR,SPIKE,OUTPUT: A series of num
+			lineList=[]
+			pallete=IBM_PALLETE[:LINE_NUM]
+			colorName=''
+			for color in pallete:
+				colorName+=(COLOR_NAME[IBM_PALLETE.index(color)])
+			AVG=sampler_3line(acfg,AVG_HIGH,AVG_LOW)
+			VAR=sampler_3line(vcfg,VAR_HIGH,VAR_LOW)
+			SPIKE=sampler_3line(scfg,[True],[False])
+			for i in range(LINE_NUM):
+				l, = plt.plot([], [], color=pallete[i])
+				xList.append(xList_create(DURATION))
+				yList.append(yList_create(i,AVG[i],VAR[i],SPIKE[i],SPIKE_H,DURATION))
+				plt.xlim(0,DURATION) #depend on Duration
+				plt.ylim(0,500) #TODO: How to determine the upper bound?
+				lineList.append(l)
+			plt.close()
+
+			writer = anime.PillowWriter(fps=50)
+
+			with writer.saving(fig, f"gif/trace/line3/trace_AVG{acfg}_VAR{vcfg}_SPIKE{scfg}_{colorName}.gif", 100):
+				for i in range(LINE_NUM):
+					for j in range(DURATION):
+						lineList[i].set_data(xList[i][:j], yList[i][:j])
+						writer.grab_frame()
+
+def sampler_4line(cfg,high_data,low_data):
+	if cfg == 'hhhh':
+		return random.sample(high_data,1)[0],random.sample(high_data,1)[0],random.sample(high_data,1)[0],random.sample(high_data,1)[0]
+	elif cfg == 'hlll':
+		return random.sample(high_data,1)[0],random.sample(low_data,1)[0],random.sample(low_data,1)[0],random.sample(low_data,1)[0]
+	elif cfg == 'lhll':
+		return random.sample(low_data,1)[0],random.sample(high_data,1)[0],random.sample(low_data,1)[0],random.sample(low_data,1)[0]
+	elif cfg == 'llhl':
+		return random.sample(low_data,1)[0],random.sample(low_data,1)[0],random.sample(high_data,1)[0],random.sample(low_data,1)[0]	
+	elif cfg == 'lllh':
+		return random.sample(low_data,1)[0],random.sample(low_data,1)[0],random.sample(low_data,1)[0],random.sample(high_data,1)[0]
+	elif cfg == 'llll':
+		return random.sample(low_data,1)[0],random.sample(low_data,1)[0],random.sample(low_data,1)[0],random.sample(low_data,1)[0]
+	else:
+		return None
+	
+LINE_NUM=4
+AVG_HIGH=[i for i in mean_list if i > np.median(mean_list)]
+AVG_LOW=[i for i in mean_list if i < np.median(mean_list)]
+AVG_CFG4=['hhhh','hlll','lhll','llhl','lllh','llll']
+VAR_HIGH=[i for i in var_list if i > np.median(var_list)]
+VAR_LOW=[i for i in var_list if i < np.median(var_list)]
+VAR_CFG4=['hhhh','hlll','lhll','llhl','lllh','llll']
+SPIKE_CFG4=['hhhh','hlll','lhll','llhl','lllh','llll']
+
+
+IBM_PALLETE=['#648FFF','#DC267F','#FE6100','#FFB000'] 
+COLOR_NAME=['b','r','o','y']
+for acfg in AVG_CFG4:
+	for vcfg in VAR_CFG4:
+		for scfg in SPIKE_CFG4:
+			fig = plt.figure()
+			xList=[] #Length of the line
+			yList=[] #function that INPUT: AVG,VAR,SPIKE,OUTPUT: A series of num
+			lineList=[]
+			pallete=IBM_PALLETE[:LINE_NUM]
+			colorName=''
+			for color in pallete:
+				colorName+=(COLOR_NAME[IBM_PALLETE.index(color)])
+			AVG=sampler_4line(acfg,AVG_HIGH,AVG_LOW)
+			VAR=sampler_4line(vcfg,VAR_HIGH,VAR_LOW)
+			SPIKE=sampler_4line(scfg,[True],[False])
+			for i in range(LINE_NUM):
+				l, = plt.plot([], [], color=pallete[i])
+				xList.append(xList_create(DURATION))
+				yList.append(yList_create(i,AVG[i],VAR[i],SPIKE[i],SPIKE_H,DURATION))
+				plt.xlim(0,DURATION) #depend on Duration
+				plt.ylim(0,500) #TODO: How to determine the upper bound?
+				lineList.append(l)
+			plt.close()
+
+			writer = anime.PillowWriter(fps=50)
+
+			with writer.saving(fig, f"gif/trace/line4/trace_AVG{acfg}_VAR{vcfg}_SPIKE{scfg}_{colorName}.gif", 100):
+				for i in range(LINE_NUM):
+					for j in range(DURATION):
+						lineList[i].set_data(xList[i][:j], yList[i][:j])
+						writer.grab_frame()
