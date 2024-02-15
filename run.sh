03767b9a9a5bc938b3bb3168c3e13ebed971b675
Delen0828 <hsw828@sjtu.edu.cn>
2024-02-15 13:27:27 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,27 +1,27 @@
-# python make.py --move stat --static True --line 2 &
-# python make.py --move stat --static True --line 3 &
-# python make.py --move stat --static True --line 4 &
+python make.py --move stat --static True --line 2 &
+python make.py --move stat --static True --line 3 &
+python make.py --move stat --static True --line 4 &
 
-# python make.py --move seq --line 2 &
-# python make.py --move seq --line 3 &
-# python make.py --move seq --line 4 &
+python make.py --move seq --line 2 &
+python make.py --move seq --line 3 &
+python make.py --move seq --line 4 &
 
-# python make.py --move seq --trace True --line 2 &
-# python make.py --move seq --trace True --line 3 &
-# python make.py --move seq --trace True --line 4 &
+python make.py --move seq --trace True --line 2 &
+python make.py --move seq --trace True --line 3 &
+python make.py --move seq --trace True --line 4 &
 
 python make.py --move seq --history True --line 2 &
 python make.py --move seq --history True --line 3 &
-python make.py --move seq --history True --line 4 
+python make.py --move seq --history True --line 4 &
 
-# python make.py --move seq --trace True --history True --line 2 &
-# python make.py --move seq --trace True --history True --line 3 &
-# python make.py --move seq --trace True --history True --line 4 &
+python make.py --move seq --trace True --history True --line 2 &
+python make.py --move seq --trace True --history True --line 3 &
+python make.py --move seq --trace True --history True --line 4 &
 
-# python make.py --move sync --line 2 &
-# python make.py --move sync --line 3 &
-# python make.py --move sync --line 4 &
+python make.py --move sync --line 2 &
+python make.py --move sync --line 3 &
+python make.py --move sync --line 4 &
 
-# python make.py --move sync --trace True --line 2 &
-# python make.py --move sync --trace True --line 3 &
-# python make.py --move sync --trace True --line 4
+python make.py --move sync --trace True --line 2 &
+python make.py --move sync --trace True --line 3 &
+python make.py --move sync --trace True --line 4
