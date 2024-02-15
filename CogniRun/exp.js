b853b3565bf12e7c3091b7ca60cd54f6129680d9
Delen0828 <hsw828@sjtu.edu.cn>
2024-02-15 17:24:20 -0500

    update

--- CogniRun/exp.js
+++ CogniRun/exp.js
@@ -106,9 +106,9 @@ var gifShow = {
     type: jsPsychImageKeyboardResponse,
     stimulus: '../testgif/'+gif_name,
     choices: "NO_KEYS",
-    prompt: "<p>Study this vis for 5 seconds.</p>",
-	render_on_canvas:false,
-    trial_duration: 5000
+    prompt: "<p>Study this vis for 8 seconds.</p>",
+	  render_on_canvas:false,
+    trial_duration: 8000
 	// stimulus_duration:4500
 };
 timeline.push(gifShow);
@@ -122,30 +122,44 @@ var Option = [];
 for (var i=0;i<line_num;i++){
 	var color=''
 	switch(match[i]){
-		case 'r' : color='Red'; break;
-		case 'y' : color='Yellow'; break;
+		case 'g' : color='Green'; break;
 		case 'b' : color='Blue'; break;
-		case 'o' : color='Orange'; break;
+		case 'y' : color='Yellow'; break;
+		case 'p' : color='Purple'; break;
 		default: color='NOT_DEFINED'; break;
 	}
 	Option.push(color);
 }
 
 Option = jsPsych.randomization.shuffle(Option);
-var attnCheck = {
+var avgCheck = {
   type: jsPsychSurveyMultiChoice,
   questions: [
     {
       prompt:
         "Which line has highest average?",
-      name: "AttnChk",
       options: Option,
       randomize_question_order: true,
       required: true,
     },
   ],
 };
-timeline.push(attnCheck);
+timeline.push(avgCheck);
+
+Option = jsPsych.randomization.shuffle(Option);
+var varCheck = {
+  type: jsPsychSurveyMultiChoice,
+  questions: [
+    {
+      prompt:
+        "Which line has highest variance?",
+      options: Option,
+      randomize_question_order: true,
+      required: true,
+    },
+  ],
+};
+timeline.push(varCheck);
 
 }
 
